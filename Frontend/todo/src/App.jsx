d281c6a170b9f291f0dbb56c48d32a85bce5fa7a
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2024-11-18 22:59:11 +0500

    Updated

--- Frontend/todo/src/App.jsx
+++ Frontend/todo/src/App.jsx
@@ -1,12 +1,12 @@
 import {Outlet} from 'react-router-dom'
 import Header from './Components/Header/Header'
-import Home from './Pages/Home'
+
 
 const App = () => {
   return (
   <>
   <Header/>
-  <Home/>
+
     <Outlet/>
     </>
   )
--- Frontend/todo/src/main.jsx
+++ Frontend/todo/src/main.jsx
@@ -4,12 +4,16 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 import Register from './Pages/Register.jsx'
 import Login from './Pages/Login.jsx'
 import App from './App.jsx'
+import Home from './Pages/Home'
 
+// ROUTING WITH PROTECTED ROUTES
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App/>}>
+      <Route index element={<Home/>}/>
       <Route path='register' element={<Register/>}/>
       <Route path='login' element={<Login/>}/>
+      <Route path='*' element={<h1>404 Page Not Found</h1>}/>
     </Route>
   )
 )
